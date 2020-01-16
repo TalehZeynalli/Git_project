7dca739126d94380be884ee76ad811fc362dac9c
Taleh Zeynalli <talehzeynalli95@gmail.com>
2020-01-16 21:39:07 +0400

    add commit

--- test_project/src/main/java/com/example/controller/TicketController.java
+++ test_project/src/main/java/com/example/controller/TicketController.java
@@ -1,9 +1,9 @@
-package com.example.controller;
+﻿package com.example.controller;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
+//deyisiklik
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
